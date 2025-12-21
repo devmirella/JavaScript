ad8ab71a7632d5426449ca972b4aae191e314fbf
devmirella <marinasillva43@gmail.com>
2025-12-21 18:52:01 -0300

    Repetição com while (Idade válida)

---
+++ ex12.js
@@ -0,0 +1,14 @@
+// Repetição com while (Idade válida)
+
+let idade = Number(prompt("Informe sua idade (0 a 120): "));
+
+while (isNaN(idade)|| idade < 0 || idade > 120){
+    alert("Idade Inválida")
+    idade = Number(prompt("Informe sua idade (0 a 120): "));
+} 
+
+if (idade >= 18){
+    console.log("Maior de Idade ");
+} else {
+    console.log("Menor de Idade ");
+}
