35ed31d6a78bf03c63c6527cf9852d54cbb5d811
devmirella <marinasillva43@gmail.com>
2025-12-23 22:34:11 -0300

    Funções

---
+++ ex15.js
@@ -0,0 +1,13 @@
+// Funções
+
+
+function calcularMedia(soma, contador) {
+    let media = soma / contador;
+    return media;
+}
+
+soma = 20;
+contador = 4;
+
+let resultado = calcularMedia(soma, contador);
+console.log("media: ", resultado);
