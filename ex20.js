e513cee04b3e3e68ec4476c1891c49bc8557fd9a
devmirella <marinasillva43@gmail.com>
2025-12-27 21:03:08 -0300

    Sistema de notas

---
+++ ex20.js
@@ -0,0 +1,30 @@
+// Sistema de notas
+
+soma = 0
+
+while (true) {
+    let nota = Number(prompt("Informe a nota ou digite 0 para encerrar:"));
+
+    if (isNaN(nota)) {
+        alert("Número inválido")
+        continue;
+
+    } else if (nota === 0) {
+        break;
+    }
+
+    soma += nota 
+}
+
+if (soma >= 80) {
+    alert("Aprovado");
+    console.log("Nota: ", soma);
+
+} else if (soma >= 60) {
+    alert("Recuperação");
+    console.log("Nota: ", soma);
+
+} else {
+    alert("Reprovado");
+    console.log("Nota: ", soma);
+}
