339e05e89ea455dd10869ac1966315536366e64e
devmirella <marinasillva43@gmail.com>
2025-12-28 23:49:09 -0300

    Site Simples

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // Ativa o modo estrito do JavaScript (evita erros silenciosos) 
 "use strict";
 
+
 // Seleciona o botão de troca de tema no HTML
 const botaoTema = document.querySelector(".botao-tema");
 
@@ -37,5 +38,49 @@ botaoTema.addEventListener("click", function() {
         // Atualiza o texto do botão 
         this.textContent = "Escuro";
     }
+
+});
+
+
+let total = 0;
+let contador = 0;
+
+// Seleciona elementos da seção de gastos
+const inputGasto = document.querySelector(".input-gasto");
+const botaoGasto = document.querySelector(".botao-gasto");
+const resultadoGastos = document.querySelector(".resultado-gastos");
+
+function atualizarResultados(total, contador) {
+    const ps = document.querySelectorAll(".resultado-gastos p");
+
+    ps[0].textContent = `Total: R$ ${total.toFixed(2)}`;
+    ps[1].textContent = `Quantidade: ${contador}`;
+    ps[2].textContent =  `Media: ${(total / contador).toFixed(2)};`
+}
+
+
+// Evento do botão "Adicionar Gasto"
+botaoGasto.addEventListener("click", function() {
+
+    const valor = Number(inputGasto.value);
+
+    // validações
+    if (isNaN(valor) || valor <= 0) {
+        alert("Digite um valor válido");
+        return;
+
+    } 
+    
+
+    // Atualiza os dados 
+    total += valor
+    contador ++;
+
+    atualizarResultados(total, contador);
+
+    // Limpa o campo
+    inputGasto.value="";
+    inputGasto.focus();
+
 
 });
