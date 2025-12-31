9e533f41b4fc6de1a34e5cae4839bd3c25edc9a1
devmirella <marinasillva43@gmail.com>
2025-12-31 13:30:17 -0300

    Conversão de dados com Number()

--- ex07.js
+++ ex07.js
@@ -1,7 +1,7 @@
 // Conversão de dados com Number()
 
 
-let idade = Number(prompt("Digite sua idade: "))
+let idade = Number(prompt("Digite sua idade: "));
 
 if (idade >= 18) {
     console.log("Você é maior de idade");
