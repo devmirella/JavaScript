91b668fa8b47ac537807ffaf7900ce7ccba0646b
devmirella <marinasillva43@gmail.com>
2025-12-31 13:40:03 -0300

    Validação com while

--- ex12.js
+++ ex12.js
@@ -3,7 +3,7 @@
 let idade = Number(prompt("Informe sua idade (0 a 120): "));
 
 while (isNaN(idade)|| idade < 0 || idade > 120){
-    alert("Idade Inválida")
+    alert("Idade Inválida");
     idade = Number(prompt("Informe sua idade (0 a 120): "));
 } 
 
